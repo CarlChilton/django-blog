2f8d0c02f363c54e5a45ecfc30965f6c74a39bb3
Carl Chilton <carlchilton@yahoo.co.uk>
2019-01-27 15:53:00 +0000

    Amended allowed hosts

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-carlchilton.c9users.io', 'blog-test-app.herokuapp.com']
+ALLOWED_HOSTS = ['django-blog-carlchilton.c9users.io', 'django-blog-cc.herokuapp.com']
 
 
 # Application definition
